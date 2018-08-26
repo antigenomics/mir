1b94f9834fdd878b1b743d5b7410d86e024adc5b
mikessh <mikhail.shugay@gmail.com>
2018-08-26 06:18:45 +0300

    embedded plain text probability converter

---
+++ src/main/java/com/milaboratory/mir/rearrangement/blocks/ConverterUtils.java
@@ -0,0 +1,109 @@
+package com.milaboratory.mir.rearrangement.blocks;
+
+import com.milaboratory.mir.segment.Segment;
+import com.milaboratory.mir.segment.SegmentLibrary;
+
+import java.util.Map;
+
+import static com.milaboratory.mir.CommonUtils.map2map;
+
+public class ConverterUtils {
+    public static <T extends Segment>
+    Map<T, Double> convertSegment(Map<String, Double> probabilities,
+                                  SegmentLibrary segmentLibrary,
+                                  Class<T> segmentClass) {
+        var segmentProvider = segmentLibrary.asSegmentProvider(segmentClass);
+        return map2map(
+                probabilities,
+                e -> segmentProvider.fromId(e.getKey()),
+                Map.Entry::getValue
+        );
+    }
+
+    public static <T extends Segment, V extends Segment>
+    Map<V, Map<T, Double>> convertSegment(Map<String, Map<String, Double>> probabilities,
+                                          SegmentLibrary segmentLibrary,
+                                          Class<T> segmentClassT,
+                                          Class<V> segmentClassV) {
+        var segmentProviderV = segmentLibrary.asSegmentProvider(segmentClassV);
+
+        return map2map(
+                probabilities,
+                e -> segmentProviderV.fromId(e.getKey()),
+                e -> convertSegment(e.getValue(), segmentLibrary, segmentClassT)
+        );
+    }
+
+    public static <T extends Segment, V extends Segment, U extends Segment>
+    Map<U, Map<V, Map<T, Double>>> convertSegment(Map<String, Map<String, Map<String, Double>>> probabilities,
+                                                  SegmentLibrary segmentLibrary,
+                                                  Class<T> segmentClassT,
+                                                  Class<V> segmentClassV,
+                                                  Class<U> segmentClassU) {
+        var segmentProviderU = segmentLibrary.asSegmentProvider(segmentClassU);
+
+        return map2map(
+                probabilities,
+                e -> segmentProviderU.fromId(e.getKey()),
+                e -> convertSegment(e.getValue(), segmentLibrary, segmentClassT, segmentClassV)
+        );
+    }
+
+    private static Map<Integer, Double> convertInt(Map<String, Double> map) {
+        return map2map(
+                map,
+                e -> Integer.parseInt(e.getKey()),
+                Map.Entry::getValue
+        );
+    }
+
+    public static <T extends Segment>
+    Map<T, Map<Integer, Double>> convertTrimming(Map<String, Map<String, Double>> probabilities,
+                                                 SegmentLibrary segmentLibrary,
+                                                 Class<T> segmentClass) {
+        var segmentProvider = segmentLibrary.asSegmentProvider(segmentClass);
+
+        return map2map(
+                probabilities,
+                e -> segmentProvider.fromId(e.getKey()),
+                e -> convertInt(e.getValue())
+        );
+    }
+
+    public static <T extends Segment>
+    Map<T, Map<Integer, Map<Integer, Double>>> convertTwoSideTrimming(Map<String, Map<String, Map<String, Double>>> probabilities,
+                                                                      SegmentLibrary segmentLibrary,
+                                                                      Class<T> segmentClass) {
+        var segmentProvider = segmentLibrary.asSegmentProvider(segmentClass);
+
+        return map2map(
+                probabilities,
+                e -> segmentProvider.fromId(e.getKey()),
+                e -> map2map(
+                        e.getValue(),
+                        ee -> Integer.parseInt(ee.getKey()),
+                        ee -> convertInt(ee.getValue())
+                )
+        );
+    }
+
+    public static Map<Byte, Double> convertNucleotide(Map<String, Double> probabilities) {
+        return map2map(
+                probabilities,
+                e -> Byte.parseByte(e.getKey()),
+                Map.Entry::getValue
+        );
+    }
+
+    public static Map<Byte, Map<Byte, Double>> convertNucleotidePair(Map<String, Map<String, Double>> probabilities) {
+        return map2map(
+                probabilities,
+                e -> Byte.parseByte(e.getKey()),
+                e -> map2map(
+                        e.getValue(),
+                        ee -> Byte.parseByte(ee.getKey()),
+                        Map.Entry::getValue
+                )
+        );
+    }
+}
