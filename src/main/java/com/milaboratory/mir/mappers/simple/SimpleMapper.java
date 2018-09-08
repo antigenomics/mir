9e294608c1f101702db53ac60413fe993a49c222
mikessh <mikhail.shugay@gmail.com>
2018-09-08 20:45:06 +0200

    2prev

--- src/main/java/com/milaboratory/mir/mappers/simple/SimpleMapper.java
+++ src/main/java/com/milaboratory/mir/mappers/simple/SimpleMapper.java
@@ -3,7 +3,6 @@ package com.milaboratory.mir.mappers.simple;
 import com.milaboratory.core.sequence.Sequence;
 import com.milaboratory.mir.mappers.HitWithAlignment;
 
-@FunctionalInterface
 public interface SimpleMapper<Q, T, S extends Sequence<S>> {
     HitWithAlignment<Q, T, S> map(Q query);
 }
