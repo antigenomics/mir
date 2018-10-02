da3507c909b513978e8df1946e53cd904e4b3e97
glennerad <alex@Y-MBPro.local>
2018-10-02 23:21:20 +0300

    Added test for ClonotypeTablePipe with Mixcr

--- src/test/java/com/milaboratory/mir/clonotype/io/ClonotypeTablePipeTest.java
+++ src/test/java/com/milaboratory/mir/clonotype/io/ClonotypeTablePipeTest.java
@@ -1,6 +1,7 @@
 package com.milaboratory.mir.clonotype.io;
 
 import com.milaboratory.mir.TestUtils;
+import com.milaboratory.mir.clonotype.parser.MixcrClonotypeParserFactory;
 import com.milaboratory.mir.segment.Gene;
 import com.milaboratory.mir.segment.Species;
 import com.milaboratory.mir.clonotype.parser.VDJtoolsClonotypeParserFactory;
@@ -14,7 +15,7 @@ import static org.junit.Assert.assertEquals;
 
 public class ClonotypeTablePipeTest {
     @Test
-    public void readTest() throws IOException {
+    public void vdjtoolsReadTest() throws IOException {
         var sampleSupplier = TestUtils.streamSupplierFrom("samples/trad_sample.txt");
         var library = MigecSegmentLibraryUtils.getLibraryFromResources(Species.Human, Gene.TRA);
         var parserFactory = new VDJtoolsClonotypeParserFactory(library, true);
@@ -34,5 +35,27 @@ public class ClonotypeTablePipeTest {
 
         clonotypeIterator = new ClonotypeTableBufferedPipe<>(sampleSupplier.get(), parserFactory);
         assertEquals(1051, clonotypeIterator.parallelStream().count());
+    }
+    @Test
+    public void mixcrReadTest() throws IOException {
+        var sampleSupplier = TestUtils.streamSupplierFrom("samples/mixcr_1.txt");
+        var library = MigecSegmentLibraryUtils.getLibraryFromResources(Species.Human, Gene.IGH);
+        var parserFactory = new MixcrClonotypeParserFactory(library, true);
+
+
+        var clonotypeIterator = new ClonotypeTablePipe<>(sampleSupplier.get(), parserFactory);
+        assertEquals(2507, clonotypeIterator.stream().count());
+
+
+        clonotypeIterator = new ClonotypeTablePipe<>(sampleSupplier.get(), parserFactory);
+        assertEquals(2507, clonotypeIterator.parallelStream().count());
+
+
+        clonotypeIterator = new ClonotypeTableBufferedPipe<>(sampleSupplier.get(), parserFactory);
+        assertEquals(2507, clonotypeIterator.stream().count());
+
+
+        clonotypeIterator = new ClonotypeTableBufferedPipe<>(sampleSupplier.get(), parserFactory);
+        assertEquals(2507, clonotypeIterator.parallelStream().count());
     }
 }
